ca1be9f81a1881d2dfe00b4cfae5d1f7ecd187f5
Harry <84292384+HarryC0des@users.noreply.github.com>
2025-02-20 03:10:04 +0000

    Adding random btn and if else statement to counting app

--- projects/counter/counter.js
+++ projects/counter/counter.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded",counterTally);
+document.addEventListener("DOMContentLoaded",trueOrFalse);
 
 function counterTally() {
     let plus = document.getElementById("plusBtn");
@@ -9,16 +10,29 @@ function counterTally() {
     plus.addEventListener("click", function(){
         let tally = parseInt(number.textContent);
         number.textContent = tally + 1;
+        trueOrFalse();
     })
     
     minus.addEventListener("click", function(){
         let tally = parseInt(number.textContent);
         number.textContent = tally - 1;
+        trueOrFalse();
     })
 
     random.addEventListener("click", function(){
         let numRand = 10*(Math.random());
         number.textContent = Math.round(numRand);
+        trueOrFalse();
     })
 }
 
+function trueOrFalse(){
+    let response = document.getElementById("answer");
+    let number = document.getElementById("counter");
+
+    if (number.textContent > 5){
+        response.textContent = "Your number is bigger than 5";
+    } else {
+        response.textContent = "Your number is smaller than 5!";
+    }
+}
