769ac8be793d94f64d8e643158fc131782df40e5
Harry <84292384+HarryC0des@users.noreply.github.com>
2025-03-06 22:25:27 -0500

    UI script updates

--- projects/tictactoe/ui.js
+++ projects/tictactoe/ui.js
@@ -1,10 +1,59 @@
+import { playGame } from "./game";
+
 function updateGame(){
+    let {computerMove} = playGame();
+    
     let topLeft = document.getElementById("rowOneLeft");
     let topMiddle = document.getElementById("rowOneMiddle");
     let topRight = document.getElementById("rowOneRight");
     let midLeft = document.getElementById("rowTwoLeft");
     let midMiddle = document.getElementById("rowTwoMiddle");
     let midRight = document.getElementById("rowTwoRight");
+    let botLeft = document.getElementById("rowThreeLeft");
+    let botMiddle = document.getElementById("rowThreeMiddle");
+    let botRight = document.getElementById("rowThreeRight");
+
+    topLeft.addEventListener("click", function(){
+        topLeft.style.backgroundColor = "red";
+        playGame("A");
+    })
+    topMiddle.addEventListener("click", function(){
+        topMiddle.style.backgroundColor = "red";
+        playGame("B");
+    })
+    topRight.addEventListener("click", function(){
+        topRight.style.backgroundColor = "red";
+        playGame("C");
+    })
+    midLeft.addEventListener("click", function(){
+        midLeft.style.backgroundColor = "red";
+        playGame("D");
+    })
+    midMiddle.addEventListener("click", function(){
+        midMiddle.style.backgroundColor = "red";
+        playGame("E");
+    })
+    midRight.addEventListener("click", function(){
+        midRight.style.backgroundColor = "red";
+        playGame("F");
+    })
+    botLeft.addEventListener("click",function(){
+        botLeft.style.backgroundColor = "red";
+        playGame("G")
+    })
+    botMiddle.addEventListener("click", function(){
+        botMiddle.style.backgroundColor = "red";
+        playGame("H");
+    })
+    botRight.addEventListener("click", function(){
+        botRight.style.backgroundColor = "red";
+        playGame("I");
+    })
+
+    computerMove.style.backgroundColor = "red";
+    
+
+
 }
 
 export {updateUI};
