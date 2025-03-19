7a810144137c1fba0d426fd5bacf02582e4309da
Harry <84292384+HarryC0des@users.noreply.github.com>
2025-03-19 01:32:04 +0000

    Making computer plays smarter

--- projects/tictactoe/game.js
+++ projects/tictactoe/game.js
@@ -20,13 +20,52 @@ function playerTurn(choice){
         return playerChoices;
     }
 
+    function findBlockingMove(playerChoices, boardChoices) {
+        let winConditions = [
+            [1, 2, 3], [4, 5, 6], [7, 8, 9], // Rows
+            [1, 4, 7], [2, 5, 8], [3, 6, 9], // Columns
+            [1, 5, 9], [3, 5, 7]            // Diagonals
+        ];
+    
+        for (let condition of winConditions) {
+            let matchCount = 0;
+            let missingNumber = null;
+    
+            for (let num of condition) {
+                if (playerChoices.includes(num)) {
+                    matchCount++; // Player has this spot
+                } else if (boardChoices.includes(num)) {
+                    missingNumber = num; // Empty spot available
+                } else {
+                    missingNumber = null; // If a number isn't free, stop checking this condition
+                    break;
+                }
+            }
+    
+            if (matchCount === 2 && missingNumber !== null) {
+                console.log(`Blocking at: ${missingNumber}`);
+                return missingNumber; // Found a blocking move
+            }
+        }
+    
+        return null; // No immediate block needed
+    };
+
+
     function computerTurn(){
+        let missingNumber = findBlockingMove(playerChoices, boardChoices);
         if (boardChoices.length === 0) return;  // Stop if no moves are left
-        let randomChoice = Math.floor(Math.random()*boardChoices.length);
-        let move = boardChoices.splice(randomChoice,1)[0];
-        computerChoices.push(move);
-        return move;
-    }
+        if(missingNumber !==null){
+            let move = missingNumber;
+            computerChoices.push(move);
+            return move;
+        } else {
+            let randomChoice = Math.floor(Math.random()*boardChoices.length);
+            let move = boardChoices.splice(randomChoice,1)[0];
+            computerChoices.push(move);
+            return move;
+        }
+    };
 
     function arrayCheck(choices,winningChoices){
         return winningChoices.some(picks =>
@@ -74,9 +113,25 @@ function playerTurn(choice){
             return gameStatus
         }
 
+        let missingNumber = findBlockingMove(playerChoices, boardChoices);
+
+
         let computerMove = computerTurn();
         gameStatus = winStatus();
-            
+
+        if(gameStatus ==="player"){
+            return
+        }
+        
+        console.log(boardChoices);
+        console.log(playerChoices);
+
+        if(missingNumber !== null){
+            console.log("the missing number is "+missingNumber);
+        } else {
+            console.log("There are no missing numbers yet");
+        };
+
         console.log(gameStatus);
         console.log(computerMove);
 
